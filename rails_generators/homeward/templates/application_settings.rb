6b3cc31befb0f845d898904ffac4d12d5c9acc98
Sandro Turriate <sandro.turriate@gmail.com>
2009-06-01 22:43:09 -0400

    simplify loading of settings.yml

--- rails_generators/homeward/templates/application_settings.rb
+++ rails_generators/homeward/templates/application_settings.rb
@@ -1,1 +1,13 @@
-APP_SETTINGS = Homeward.load_app_config
+def load_app_settings
+  returning HashWithIndifferentAccess.new do |app_settings|
+    config_file_path = File.join(RAILS_ROOT, %w(config settings.yml))
+    if File.exist?(config_file_path)
+      config = YAML.load_file(config_file_path)
+      app_settings.merge!(config[Rails.env]) if config[Rails.env]
+    else
+      puts "WARNING: configuration file #{config_file_path} not found."
+    end
+  end
+end
+
+APP_SETTINGS = load_app_settings
